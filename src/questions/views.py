259efcc722c1b239b9dab0878c8b37ea0f358581
Nishant Sharma <nishant-ai@outlook.com>
2021-08-02 17:37:58 +0530

    Rectified project

--- src/questions/views.py
+++ src/questions/views.py
@@ -1,5 +1,24 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import QuesIntType, QuesMcq
+import json
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'quiz.html', context={})
+    questions_mcq = QuesMcq.objects.all()
+    questions_int_type = QuesIntType.objects.all()
+    
+    question_list = []
+    for ques in questions_mcq:
+        a = {
+            'question' : ques.question,
+            'option_1' : ques.option_1,
+            'option_2' : ques.option_2,
+            'option_3' : ques.option_3,
+            'option_4' : ques.option_4,
+            'answer' : ques.answer
+        }
+        question_list.append(a)
+        # print(question_list)
+    
+    return render(request, 'quiz.html', {'question_list' : json.dumps(question_list)})
