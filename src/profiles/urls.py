a9a7134c02b13b903a920a12b85e14fd172593fc
Nishant Sharma <nishant-ai@outlook.com>
2021-08-03 15:51:52 +0530

    Added Online/Offline status 'wsgi'

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import profiles_home
+from .views import profiles_home, profiles_login, profiles_logout
 
 urlpatterns = [
-    path('', profiles_home, name='profiles-home')
+    path('', profiles_home, name='profiles-home'),
+    path('login/', profiles_login, name='profiles-login'),
+    path('logout/', profiles_logout, name='profiles-logout')
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,48 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render
+
+from django.contrib.auth.models import auth
+from .models import Profile
 
 # Create your views here.
+
+# When Login Successful set online = True
+def profiles_login(request):
+
+    if request.method == 'POST':
+        user = request.POST['user']
+        password = request.POST['password']
+        print(user + " - " + password)
+        
+        user_User = auth.authenticate(username=user, password=password)
+        if user_User is not None:
+            auth.login(request, user_User)
+            
+            user_Profile = Profile.objects.filter(pk=user_User.id)
+            user_Profile.update(is_online=True)
+            
+            print('Login Successful')
+            
+            return redirect('profiles-home')
+        else:
+            print('Invalid Creds')
+    
+    context = {}
+    return render(request, 'login.html', context)
+
+# When Logout, online = False
+def profiles_logout(request):
+    if request.user.is_authenticated:
+        user_Profile = Profile.objects.filter(pk=request.user.pk)
+        user_Profile.update(is_online=False)
+        auth.logout(request)
+        return HttpResponse('User Logged Out Successfully')
+    else:
+        return HttpResponse('No User was logged-in')
+    
+# Home Page where Logged In users are shown
 def profiles_home(request):
-    return HttpResponse("<h3> Profiles Home Page </h3>")
+    loggedin_users = Profile.objects.filter(is_online=True)
+        
+    context = {'loggedin_users' : loggedin_users}
+    return render(request ,'profiles.html', context)
